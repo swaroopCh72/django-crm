bb47fbfa7b84f3ed7a19020197dc6df853c05f88
swaroopCh72 <221663459+swaroopCh72@users.noreply.github.com>
2026-08-26 17:56:39 +0530

    Docker Volume Integration

--- CRMProject/CRMProject/settings.py
+++ CRMProject/CRMProject/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR.parent / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -76,7 +80,7 @@ WSGI_APPLICATION = 'CRMProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     }
 }
 
